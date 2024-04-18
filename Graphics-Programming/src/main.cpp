ead32b7f16eccb27895617ac93c7f8fad6aa7264
Watenk <ivar.nuij@gmail.com>
2024-04-18 23:49:23 +0200

    Split code into functions

--- Graphics-Programming/src/main.cpp
+++ Graphics-Programming/src/main.cpp
@@ -3,17 +3,51 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main(){
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+// Forward Declaration
+int init(GLFWwindow* &window, const char* windowName);
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);  
+void processInput(GLFWwindow *window);
+void createTriangle();
+void createShaders();
 
+int main(){
+
+    GLFWwindow* window;
+    int res = init(window, "Graphics Programming");
+    if (res != 0) return res;
+
+    createTriangle();
+    createShaders();
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)){
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT); // Clear color buffer
+        processInput(window);
+        glfwPollEvents(); // Windows Window Events
+
+
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+int init(GLFWwindow* &window, const char* windowName){
+
+    /* Init Lib */
+    if (!glfwInit()) return -1;
+
+    /* Winow Hints */ 
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
-    /* Create a windowed mode window and its OpenGL context */
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    /* Create a windowed mode window and its OpenGL context */ 
+    window = glfwCreateWindow(640, 480, windowName, NULL, NULL);
     if (!window){
         std::cout << "Failed to create glfw window" << std::endl;
         glfwTerminate();
@@ -23,24 +57,31 @@ int main(){
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    /* Set callback for window size change */
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);  
+
+    /* Load GLAD */
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
         std::cout << "Failed to load GLAD" << std::endl;
         glfwTerminate();
         return -2;
     }
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window)){
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
     return 0;
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height){
+    glViewport(0, 0, width, height);
+}  
+
+void processInput(GLFWwindow *window){
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void createTriangle(){
+
+}
+
+void createShaders(){
+
 }
