5e97722300bd065445676879699ef5d986f61e73
Watenk <ivar.nuij@gmail.com>
2024-05-03 00:37:40 +0200

    Added glm

--- Graphics-Programming/src/main.cpp
+++ Graphics-Programming/src/main.cpp
@@ -3,6 +3,9 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "Shader.h"
 #include "Mesh.h"
@@ -64,7 +67,14 @@ int main(){
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT); // Clear color buffer
         processInput(window);
-        
+
+        // // RotateScaleTranslate
+        // glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+        // glm::mat4 trans = glm::mat4(1.0f);
+        // trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+        // vec = trans * vec;
+        // std::cout << vec.x << vec.y << vec.z << std::endl;
+
         // Draw ----------------
 
         quadMesh.bind();
